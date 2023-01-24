e2f084dba43ff2924fb388e54ee5f6cf76de54ff
bruano95 <bruano@uw.edu>
2023-01-24 14:50:45 -0800

    Changes to CSSand HTML

---
+++ script.js
@@ -0,0 +1,93 @@
+var quizData = [
+    {
+        question: 'Who is the Single Season Homerun leader?',
+        a: 'Sammy Sosa',
+        b: 'Barry Bonds',
+        c: 'Mark Mcquire',
+        d: 'Ichiro',
+        correct: 'b'
+    },
+    {
+        question: 'Who won the 2022 World Series?',
+        a: 'Seattle Mariners',
+        b: 'New York Yankees',
+        c: 'Los Angeles Dodgers',
+        d: 'Houston Astros',
+        correct: 'd'
+    },
+    {
+        question: 'What does RBI stand for?',
+        a: 'Runs batted in',
+        b: 'Runners both in-play',
+        c: 'Rare baseball information',
+        d: 'None of the above',
+        correct: 'a'
+    },
+    {
+        question: 'Who is the 2022 AL MVP?',
+        a: 'Julio Rodriguez',
+        b: 'Juan Soto',
+        c: 'Aaron Judge',
+        d: 'Mike Trout',
+        correct: 'c'
+    }
+];
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+const timeCount = document.querySelector(".timer_sec")
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEls.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEls => {
+        if(answerEls.checked) {
+            answer = answerEls.id
+        }
+    })
+    return answer
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++
+        }
+        currentQuiz++
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            currentQuiz.innerHtml = `
+            <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+            `
+        }
+    }
+})
